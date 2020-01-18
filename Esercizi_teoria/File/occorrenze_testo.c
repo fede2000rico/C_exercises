f84f0425bf78cb2214cfe70ba74eceb3e3e83ac6
federicopiol <fede2000rico@users.noreply.github.com>
2020-01-18 21:52:48 +0100

    Esercizi_teoria\File\occorrenze_testo.c

---
+++ Esercizi_teoria/File/occorrenze_testo.c
@@ -0,0 +1,23 @@
+//Conta le occorrenze delle 26 lettere minuscole dell'alfabeto, ignorando gli altri caratteri
+#include<stdio.h>
+
+//Array per contare le occorrenze delle lettere
+int alfabeto[26]={0};
+
+int main(){
+  //Creo e apro il file in modalità lettura
+  FILE *fp;
+  fp=fopen("percorso file","r");
+
+  //Ciclo su tutto il contenuto del file
+  while(!feof(fp)){
+    char val;
+    //Scannerizzo ogni carattere
+    fscanf(fp,"%c",&val);
+
+    //Verifico che sia nel range valido ed incremento l'array alfabeto
+    if(val-'a'>=0 && val-'a'<26){
+      alfabeto[val]++;
+    }
+  }
+}
